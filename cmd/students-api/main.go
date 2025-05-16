6cbb8bee71477426568faf9af3f38bf3172004d1
nakul-krishnakumar <nakulkrishnakumar86@gmail.com>
2025-05-16 07:07:00 +0000

    setup basic http server

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,5 +1,59 @@
 package main
 
+import (
+	"context"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/nakul-krishnakumar/go-http/internal/config"
+)
+
 func main() {
+	// load config
+	cfg := config.MustLoad()
+	// database setup
+	// setup router
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("welcome to students api"))
+	})
+
+	// setup server
+	server := http.Server {
+		Addr: cfg.Addr,
+		Handler: router,
+	}
+
+	slog.Info("[INFO] server started", "url", "http://"+cfg.Addr)
 	
+	//* graceful shutdown
+	done := make(chan os.Signal, 1)
+
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil{
+			slog.Error("failed to start server")
+		}
+	} ()
+
+	<- done
+
+	slog.Info("shutting down the server")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		slog.Error("failed to shutdown server", slog.String("error", err.Error()))
+	}
+
+	slog.Info("server shutdown successfully")
+
 }
